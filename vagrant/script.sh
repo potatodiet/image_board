0ff10d9f39950d004095b11bc9e66344d9fb8de1
pyrohail <justin@pyrohail.com>
2014-10-10 21:38:43 -0400

    Update Vagrantfiles

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-apt-get install postgresql postgresql-contrib libpq-dev -y # Needed for postgresql
-apt-get install nfs-common portmap -y # Needed for nfs
-apt-get install nodejs -y # Needed for asset pipeline
-apt-get install imagemagick -y # Needed for carrierwave
-apt-get install ruby-dev -y # Needed for rails
-apt-get install build-essential g++ -y # Needed for thin
-
-gem install bundler
-
 sudo -u postgres createuser vagrant --createdb
-
-cd /home/vagrant/code/image_board
-sudo -u vagrant bundle install
-sudo -u vagrant rake db:create db:migrate
