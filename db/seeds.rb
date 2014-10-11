087cbb39b4007cf2e6db6ad0202e61689f4a9eda
pyrohail <justin@pyrohail.com>
2014-10-11 16:42:57 -0400

    Forgot to fully update seeds for devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(username: "Admin", email: "test@test.test" ,password: "Password123", role: "admin")
+User.create(name: "Admin", email: "test@test.test", password: "Password123", role: "admin")
